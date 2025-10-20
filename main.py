0b228dde1e1be0ba8c245d6f038933ee072f642e
Kyle Cranney <k.cranney97@gmail.com>
2025-10-19 21:10:59 -0400

    5: refactoring 1

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import get_num_words
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
@@ -7,10 +9,6 @@ def main():
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-def get_num_words(text):
-    words = text.split()
-    return f"Found {len(words)} total words."
 
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(text):
+    words = text.split()
+    return f"Found {len(words)} total words."
