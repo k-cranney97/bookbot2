cd20c18565cdaa4a65e530c0d4ee628e920e41af
Kyle Cranney <k.cranney97@gmail.com>
2025-10-19 21:07:06 -0400

    4: count of words in text

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
-
+    result = get_num_words(text)
+    print(result)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_num_words(text):
+    words = text.split()
+    return f"Found {len(words)} total words."
 
 
 main()
