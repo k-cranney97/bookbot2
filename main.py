fbf25dc773962b28372e74fc2b592baa2a1da5f7
Kyle Cranney <k.cranney97@gmail.com>
2025-10-19 21:47:42 -0400

    6: added character count

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from stats import get_num_words
+from stats import get_num_characters
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     result = get_num_words(text)
+    num_char = get_num_characters(text)
     print(result)
+    print(num_char)
+
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_num_words(text):
     words = text.split()
     return f"Found {len(words)} total words."
+
+def get_num_characters(text):
+    total = {}
+    lower_text = text.lower()
+    for char in lower_text:
+        if char not in total:
+            total[char] = 1
+        else:
+            total[char] += 1
+    return total
